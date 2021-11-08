5a9489eaf1ea1d0c9b171b5e57c50880771eb52c
AeeUX <andreaseriksen99@hotmail.com>
2021-11-08 16:59:59 +0000

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 
+//Det her eksempel bruger styled-components
+
+/* husk at Funktionens navn ikke må være ens 
+med consts fra styled-components */
+
+/*
+!!Eksempel!!
+Vi har funktionen ( Const Button = styled.button )
+Vi har også RFCE function som hedder Knap, og hvis vi kaldte 
+den 'Button' ville vi have declared en funktion x2
+og det vil den ikke have, så DONT DO IT :P
+*/
+
+function Knap() {
+  return (
+  <Container>
+    <Button>Normal Button</Button>
+    <Button primary>Primary Button</Button>
+  </Container>
+  )
+}
+
+export default Knap
+
 const Button = styled.button`
   background: transparent;
   border-radius: 3px;
@@ -20,14 +44,3 @@ const Button = styled.button`
 const Container = styled.div`
   text-align: center;
 `
-
-function Knap() {
-  return (
-  <Container>
-    <Button>Normal Button</Button>
-    <Button primary>Primary Button</Button>
-  </Container>
-  )
-}
-
-export default Knap
