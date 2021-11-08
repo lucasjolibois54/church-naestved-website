8b4d5b6c2b53fd3648bdbb9297caaee12a9c2995
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-08 20:30:49 +0100

    Create ValuesSec.js

---
+++ src/components/ValuesSec.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/global.css';
+
+//Det her eksempel bruger TailwindCSS
+
+//Husk at importere '../styles/global.css'
+//Herefter laver du en className attribute
+//Til sidst definerer du TailwindCss værdierne
+//Velbekommen <3<3
+
+function ValueSec() {
+    return (
+        <div>
+            <button className="p-6 bg-red-500 w-20">Click Me!</button>
+        </div>
+    )
+}
+
+export default ValueSec
