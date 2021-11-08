a24695f8b301a6003fb13bd5bfd477f7ada3d32e
Casper <casp8249@edu.easj.dk>
2021-11-08 21:41:23 +0100

    hand position

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -15,18 +15,18 @@ function Hand() {
       <div className="w-3/5">
         <img src={barn} className="h-full" />
       </div>
-      <div className="w-2/5 pl-10">
-        <h3 className="text-2xl">Hånden på hjertet</h3>
-        <h1 className="text-5xl">Til dig mellem 10 til 20 år</h1>
+      <div className="flex flex-col w-2/5 pl-10 pr-4 justify-center">
+        <h3 className="text-2xl font-semibold">Hånden på hjertet</h3>
+        <h1 className="text-7xl font-bold">Til dig mellem 10 til 20 år</h1>
         <p className="text-xl">
           Går du og bekymrer dig, er ked af det og måske har brug for at snakke
           med nogen?
         </p>
-        <h3 className="text-2xl">
+        <h3 className="text-2xl font-semibold">
           Så ring til <span>23808237</span> <br /> Vi har åbent mandag, onsdag
           og fredag mellem kl. 16 - 18
         </h3>
-        <a>Eller kontakt os anonymt</a>
+        <a className="font-semibold">Eller kontakt os anonymt</a>
       </div>
     </div>
   );
