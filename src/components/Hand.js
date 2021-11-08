97f9b3381f02ec7eb9414950c7f5ccb45386fbd7
Casper <casp8249@edu.easj.dk>
2021-11-08 21:58:59 +0100

    responsive pt. 1

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -11,22 +11,28 @@ import barn from "../images/barn.png";
 
 function Hand() {
   return (
-    <div className="flex w-full">
-      <div className="w-3/5">
+    <div className="flex flex-col w-full lg:flex-row">
+      <div className="w-full lg:w-3/5">
         <img src={barn} className="h-full" />
       </div>
-      <div className="flex flex-col w-2/5 pl-10 pr-4 justify-center">
-        <h3 className="text-2xl font-semibold">Hånden på hjertet</h3>
-        <h1 className="text-7xl font-bold">Til dig mellem 10 til 20 år</h1>
-        <p className="text-xl">
+      <div className="flex flex-col w-full pl-10 pr-4 justify-center lg:w-2/5">
+        <h3 className="text-2xl font-semibold text-regular-green">
+          Hånden på hjertet
+        </h3>
+        <h1 className="text-7xl font-bold text-dark-text">
+          Til dig mellem
+          <span className="text-regular-green"> 10 til 20 år</span>
+        </h1>
+        <p className="text-xl text-dark-text">
           Går du og bekymrer dig, er ked af det og måske har brug for at snakke
           med nogen?
         </p>
-        <h3 className="text-2xl font-semibold">
-          Så ring til <span>23808237</span> <br /> Vi har åbent mandag, onsdag
-          og fredag mellem kl. 16 - 18
+        <h3 className="text-2xl font-semibold text-dark-text">
+          Så ring til{" "}
+          <span className="text-regular-green font-bold">23808237</span>. <br />
+          Vi har åbent mandag, onsdag og fredag mellem kl. 16 - 18
         </h3>
-        <a className="font-semibold">Eller kontakt os anonymt</a>
+        <a className="font-semibold ">Eller kontakt os anonymt</a>
       </div>
     </div>
   );
