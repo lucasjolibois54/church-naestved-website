8198c65684701a44e5c2d3a0f919e131e4aefe0a
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-08 22:47:20 +0100

    Update ValuesSec.js

--- src/components/ValuesSec.js
+++ src/components/ValuesSec.js
@@ -22,17 +22,17 @@ function ValueSec() {
         "
       >
         <div className="w-full bg-light-green pt-10 pb-20 sm:w-2/6">
-          <h2 className="pl-8 text-4xl font-black pb-10 text-biege-text">Barmhjertighed</h2>
+          <h2 className="pl-8 text-4xl font-black pb-10 text-biege-text md:text-5xl">Barmhjertighed</h2>
           <h2 className="pl-10 text-2xl text-biege-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</h2>
         </div>
         <div className="w-full bg-medium-green pt-10 pb-20 sm:w-2/6">
-          <h2 className="pl-8 text-5xl pb-10 font-black text-biege-text">kærlighed</h2>
+          <h2 className="pl-8 text-4xl pb-10 font-black text-biege-text  md:text-5xl">kærlighed</h2>
           <h2 className="pl-10 pr-10 text-2xl text-biege-text">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
           </h2>
         </div>
         <div className="w-full bg-dark-green pt-10 pb-20 sm:w-2/6">
-          <h2 className="pl-8 text-5xl pb-10 font-black text-biege-text ">velgørenhed</h2>
+          <h2 className="pl-8 text-4xl pb-10 font-black text-biege-text  md:text-5xl">velgørenhed</h2>
           <h2 className="pl-10 text-2xl text-biege-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</h2>
         </div>
       </div>
