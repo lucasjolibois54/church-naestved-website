c960bb67cdfd13afded39945661689c06c1c1d46
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-10 15:23:30 +0100

    Update AboutHome.js

--- src/components/AboutHome.js
+++ src/components/AboutHome.js
@@ -21,7 +21,7 @@ function AboutHome() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
           </p>
           <h3 className="text-xl font-semibold text-dark-text pb-5 pt-2 flex flex-col items-center md:flex-row md:text-2xl"><span className="mr-3 mb-4 md:mb-0">
-          Find us on social media:{" "}</span>
+          Find os på sociale medier:{" "}</span>
             <span className="text-regular-green font-bold flex"><img src={fb} className=" h-auto w-7 mr-3" /><img src={ig} className=" h-auto w-7  mr-3" /><img src={tw} className=" h-auto w-7  mr-3" /><img src={yt} className=" h-auto w-7" /></span>
           </h3>
           <div className="flex justify-center text-center items-center py-0 xl:justify-start">
