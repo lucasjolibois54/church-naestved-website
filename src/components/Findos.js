0081b3d3d261a4d6e93625dc68c8a57b8ea874d5
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-10 19:38:57 +0100

    Update Findos.js

--- src/components/Findos.js
+++ src/components/Findos.js
@@ -4,7 +4,7 @@ import Kirke from "../images/Krike.png";
 
 function Findos() {
     return (
-        <div className=" p-0 flex flex-col w-full xl:flex-row md:p-16 mb-4">
+        <div className=" p-0 mt-16 flex flex-col w-full xl:flex-row md:p-16 mb-4">
             <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
                 <h3 className="text-2xl pt-5 font-semibold text-regular-green">
                     Informationer
@@ -21,11 +21,11 @@ function Findos() {
 
                 </p>
 
-
+            
             </div>
             <div className="w-full xl:w-5/10">
                 <div className="items-center grid place-items-center">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2275.602893083584!2d11.786574!3d55.22518900000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46529769d9971641%3A0xf02bfa1717118a36!2sN%C3%A6stved%20Provsti!5e0!3m2!1sen!2sdk!4v1636562656241!5m2!1sen!2sdk" width="600" height="450" allowfullscreen="" loading="lazy"></iframe>
+            <iframe className="w-90% h-72 md:w-4/5 lg:w-full h-96" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2275.602893083584!2d11.786574!3d55.22518900000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46529769d9971641%3A0xf02bfa1717118a36!2sN%C3%A6stved%20Provsti!5e0!3m2!1sen!2sdk!4v1636562656241!5m2!1sen!2sdk" allowfullscreen="" loading="lazy"></iframe>
             </div>
             </div>
         </div>
