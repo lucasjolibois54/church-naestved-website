116f4eb5f69cde4f0d6a71026e2701939d7a36c3
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-10 13:14:22 +0100

    Update AboutHome.js

--- src/components/AboutHome.js
+++ src/components/AboutHome.js
@@ -13,7 +13,7 @@ function AboutHome() {
           <h3 className="text-2xl pt-10 font-semibold text-regular-green">
           Mere om os
           </h3>
-          <h2 className="text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans text-dark-text py-5">
+          <h2 className="text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans text-dark-text py-3">
             Lær mere at <br />
             <span className="text-regular-green"> kende om os</span>
           </h2>
@@ -24,7 +24,7 @@ function AboutHome() {
           Find us on social media:{" "}</span>
             <span className="text-regular-green font-bold flex"><img src={fb} className=" h-auto w-7 mr-3" /><img src={ig} className=" h-auto w-7  mr-3" /><img src={tw} className=" h-auto w-7  mr-3" /><img src={yt} className=" h-auto w-7" /></span>
           </h3>
-          <div className="flex justify-center text-center items-center py-8 xl:justify-start">
+          <div className="flex justify-center text-center items-center py-0 xl:justify-start">
             <a
               href="https://tailwindcss.com/docs/responsive-design"
               className="font-semibold bg-dark-green px-4 py-2 text-biege-text rounded"
