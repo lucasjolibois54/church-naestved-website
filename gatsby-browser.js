2070f4fc386e1e6039c5ee615f2db910fbfc00e9
AeeUX <andreaseriksen99@hotmail.com>
2021-11-10 11:44:15 +0000

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,8 +3,7 @@ import './src/styles/global.css';
 import React from 'react';
 import { AnimateSharedLayout } from 'framer-motion';
 
-export const wrapPageElement = ({ element })
-  => (
+export const wrapPageElement = ({ element }) => (
   <AnimateSharedLayout>
     {element}
   </AnimateSharedLayout>
