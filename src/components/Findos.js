4a93772966186e0e92baa380a6b9cba5c265c77e
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-10 19:40:41 +0100

    Update Findos.js

--- src/components/Findos.js
+++ src/components/Findos.js
@@ -25,7 +25,7 @@ function Findos() {
             </div>
             <div className="w-full xl:w-5/10">
                 <div className="items-center grid place-items-center">
-            <iframe className="w-90% h-72 md:w-4/5 lg:w-full h-96" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2275.602893083584!2d11.786574!3d55.22518900000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46529769d9971641%3A0xf02bfa1717118a36!2sN%C3%A6stved%20Provsti!5e0!3m2!1sen!2sdk!4v1636562656241!5m2!1sen!2sdk" allowfullscreen="" loading="lazy"></iframe>
+            <iframe className="w-90% h-72 md:w-4/5 lg:w-full lg:h-96" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2275.602893083584!2d11.786574!3d55.22518900000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46529769d9971641%3A0xf02bfa1717118a36!2sN%C3%A6stved%20Provsti!5e0!3m2!1sen!2sdk!4v1636562656241!5m2!1sen!2sdk" allowfullscreen="" loading="lazy"></iframe>
             </div>
             </div>
         </div>
