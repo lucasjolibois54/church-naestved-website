e77943a0deba8deac3b0903f3e8cd542639d4159
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-10 22:05:46 +0100

    Update Findos.js

--- src/components/Findos.js
+++ src/components/Findos.js
@@ -9,7 +9,7 @@ function Findos() {
         Aos.init({ duration: 2000 });
     }, []);
     return (
-        <div className=" p-0 mt-24 flex flex-col w-full xl:flex-row md:p-16 mb-4">
+        <div className=" p-0 mt-24 mb-32 flex flex-col w-full xl:flex-row md:p-16 ">
             <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
                 <h3 data-aos="fade-right" className="text-2xl pt-5 font-semibold text-regular-green">
                     Informationer
