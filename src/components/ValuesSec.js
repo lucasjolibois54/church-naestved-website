10aa27b5f6c65b84511c3e46fd8cba53c2fc7232
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-10 15:53:51 +0100

    Update ValuesSec.js

--- src/components/ValuesSec.js
+++ src/components/ValuesSec.js
@@ -28,14 +28,14 @@ at tage sig af hinanden og vise omsorg
 er en vigtig del af det vi står for.</h2>
         </div>
         <div className="w-full bg-medium-green pt-10 pb-20 sm:w-2/6">
-          <h2 className="pl-8 text-4xl pb-10 font-black text-biege-text  md:text-5xl">kærlighed</h2>
+          <h2 className="pl-8 text-4xl pb-10 font-black text-biege-text  md:text-5xl">Kærlighed</h2>
           <h2 className="pl-10 pr-10 text-xl text-biege-text">
           Kærlighed for os er som en varm inderlig følelse af hengivenhed og respekt 
 vi viser til andre
           </h2>
         </div>
         <div className="w-full bg-dark-green pt-10 pb-20 sm:w-2/6">
-          <h2 className="pl-8 text-4xl pb-10 font-black text-biege-text  md:text-5xl">velgørenhed</h2>
+          <h2 className="pl-8 text-4xl pb-10 font-black text-biege-text  md:text-5xl">Velgørenhed</h2>
           <h2 className="pl-10 text-xl text-biege-text">Udøvelse af velgørenhed er for os en frivillig ydelse af hjælp til nødlidende, som en humanitær handling.</h2>
         </div>
       </div>
