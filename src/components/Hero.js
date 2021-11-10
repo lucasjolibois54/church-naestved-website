a90ebca2b46d8878b26816f5cd7046cd62d280a6
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-10 15:55:31 +0100

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -24,7 +24,7 @@ function Hero() {
                         <p className="mt-2 md:text-right text-sm text-dark-text md:text-lg text-center">Det hellige Evangelium er hverdag hos os. Vi befinder os både som din fysiske og digitale kirke med alt fra events med koncerter, arrangementer til gudstjenester og meget mere. </p>
                         <div className="flex space-x-3 justify-center md:justify-end lg:justify-end mt-6">
                             <a href="https://www.facebook.com/FontGen/" className="bg-dark-green px-14 py-6 md:font-semibold text-biege-text inline-flex items-center space-x-2 rounded">
-                                <span className="text-3xl">LEARN MORE ➡</span>
+                                <span className="text-3xl">Få mere at vide &rarr;</span>
                             </a>
                         </div>
                     </div>
