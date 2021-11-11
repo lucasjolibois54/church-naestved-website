1e965ee3454bf2f7e1284607893f1b487a752628
AeeUX <andreaseriksen99@hotmail.com>
2021-11-11 14:47:53 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,18 @@ module.exports = {
       __key: "pages",
     },
     `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
 };
