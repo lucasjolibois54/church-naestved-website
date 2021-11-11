e623a15f6da0e1305ad40649a9e9956b37704a27
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-11 16:14:06 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,17 +29,6 @@ module.exports = {
       __key: "pages",
     },
     `gatsby-transformer-remark`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-      },
-    },
+    
   ],
 };
