129fc47b37ade3090f660ebcf9829f9678dbb909
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-13 15:34:58 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.naestved-provsti.netlify.app/",
     title: "church",
   },
   plugins: [
